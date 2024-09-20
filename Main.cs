f482e56366e7f281789ba33f044282686ce085dc
kevin.lin93 <kevin.lin@titansoft.com.sg>
2024-09-21 05:05:12 +0800

    init

---
+++ Main.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using Flow.Launcher.Plugin;
+
+namespace WebStormWorkFlow;
+
+public class Main : IPlugin
+{
+    private PluginInitContext _context;
+    private Dictionary<string, string> _projects;
+
+    public void Init(PluginInitContext context)
+    {
+        _context = context;
+        LoadProjects();
+    }
+
+    public List<Result> Query(Query query)
+    {
+        var results = new List<Result>();
+        var keyword = query.Search;
+
+        foreach (var project in _projects)
+        {
+            if (project.Key.ToLower().Contains(keyword.ToLower()))
+            {
+                results.Add(new Result
+                {
+                    Title = project.Key,
+                    SubTitle = $"Open {project.Key} with WebStorm",
+                    IcoPath = "Images\\icon.png",
+                    Action = e =>
+                    {
+                        OpenProject(project.Value);
+                        return true;
+                    }
+                });
+            }
+        }
+
+        return results;
+    }
+
+    private void LoadProjects()
+    {
+        const string projectsDirectory = @"D:\projects";
+        _projects = new Dictionary<string, string>();
+
+        foreach (var dir in Directory.GetDirectories(projectsDirectory))
+        {
+            var dirName = Path.GetFileName(dir);
+            _projects[dirName] = dir;
+        }
+    }
+
+    private void OpenProject(string path)
+    {
+        const string webstormExecutable = @"C:\Users\kevin.lin93\AppData\Local\Programs\oh-my-posh\bin\webstorm.cmd";
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = webstormExecutable,
+            Arguments = $"\"{path}\"",
+            UseShellExecute = true
+        });
+    }
+}
